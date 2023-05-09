6e31a35479144f39bb163e02ef0ee1365f374b3c
Mathieu Chambaud <mathieu.chambaud@niji.fr>
2023-05-09 17:21:01 +0200

    add project

---
+++ PantryApp/PantryApp/PantryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryAppApp.swift
+//  PantryApp
+//
+//  Created by Mathieu CHAMBAUD on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PantryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
